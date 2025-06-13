aa1128fed6a2159ef39bf332624dc01c539b3a6e
“soniardza” <sonia.rdzalv@gmail.com>
2025-06-13 14:54:47 -0600

    Solving the exercise 1

--- app/src/main/java/com/soniadevs/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/soniadevs/myfirstcomposeapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.soniadevs.myfirstcomposeapp.components.layout.ExerciseOne
 import com.soniadevs.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstComposeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ExerciseOne(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
