064256610cbf2b9998ba4b2d41207d468d32b34b
“soniardza” <sonia.rdzalv@gmail.com>
2025-06-13 15:34:18 -0600

    Learning Basic constraintLayout

---
+++ app/src/main/java/com/soniadevs/myfirstcomposeapp/components/layout/MyConstraintLayout.kt
@@ -0,0 +1,41 @@
+package com.soniadevs.myfirstcomposeapp.components.layout
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Composable
+fun MyBasicConstraintLayout(modifier: Modifier) {
+    ConstraintLayout(modifier = modifier.fillMaxSize()) {
+        val (boxRed, boxGray, boxGreen, boxMagenta, boxYellow) = createRefs()
+
+        Box(Modifier.size(150.dp).background(Color.Red).constrainAs(boxRed){
+            top.linkTo(boxYellow.bottom)
+            start.linkTo(boxYellow.end)
+        })
+        Box(Modifier.size(150.dp).background(Color.Gray).constrainAs(boxGray){
+            top.linkTo(boxYellow.bottom)
+            end.linkTo(boxYellow.start)
+        })
+        Box(Modifier.size(150.dp).background(Color.Green).constrainAs(boxGreen){
+            start.linkTo(boxYellow.end)
+            bottom.linkTo(boxYellow.top)
+        })
+        Box(Modifier.size(150.dp).background(Color.Magenta).constrainAs(boxMagenta){
+            end.linkTo(boxYellow.start)
+            bottom.linkTo(boxYellow.top)
+        })
+        Box(Modifier.size(150.dp).background(Color.Yellow).constrainAs(boxYellow){
+            bottom.linkTo(parent.bottom)
+            end.linkTo(parent.end)
+            start.linkTo(parent.start)
+            top.linkTo(parent.top)
+        })
+    }
+}
